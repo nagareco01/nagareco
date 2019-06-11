e4b92b7f900ad5248ab0d86bb2616d13713e470e
sawadashutaro <shushutarou@outlook.jp>
2019-06-11 16:23:06 +0900

    add index

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,4 +3,9 @@ class Client < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders
+  has_many :addresses
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+
+	belongs_to :client
+	belongs_to :cd
+
 end
