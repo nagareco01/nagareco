6305336a1253ce6b404bb3a05396befaf5577715
watanabe055 <mailok1212@gmail.com>
2019-06-12 08:12:09 +0000

    cd_new_create

--- app/controllers/admins/cds_controller.rb
+++ app/controllers/admins/cds_controller.rb
@@ -1,5 +1,6 @@
 class Admins::CdsController < ApplicationController
   def new
+    @cd = Cd.new
   end
 
   def show
@@ -12,11 +13,17 @@ class Admins::CdsController < ApplicationController
   end
 
   def create
+    @cd = Cd.new
+    cd.save
   end
 
   def update
   end
 
   def destroy
+  end
+
+  def cd_params
+    params.require(:cd).permit(:name, :artist, :price, :stock,)
   end
 end
