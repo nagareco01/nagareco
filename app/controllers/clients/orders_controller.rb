eb26967915d7a6eebc48386cb3038f400a36a8f7
sawadashutaro <shushutarou@outlook.jp>
2019-06-12 12:19:41 +0900

    add action

--- app/controllers/clients/orders_controller.rb
+++ app/controllers/clients/orders_controller.rb
@@ -5,5 +5,14 @@ class Clients::OrdersController < ApplicationController
   end
 
   def purchase
+  	post = Order.new(order_params)
+  	post.save
+  	redirect_to clients_path
+  end
+
+  private
+
+  def order_params
+  	params.require(:order).permit(:payment, :total_price, :shipping_name, :shipping_post_code, :shipping_address)
   end
 end
