4530da60a67d46798b96f34b10acb7c1eb789337
NTM-M <ntm.m9898@gmail.com>
2019-06-12 05:29:02 +0000

    [create]mypege

--- app/controllers/clients/clients_controller.rb
+++ app/controllers/clients/clients_controller.rb
@@ -1,10 +1,17 @@
 class Clients::ClientsController < ApplicationController
+	befor_action :authenticate_client!
+
+
   def show
+  	@client = Client.find(params[:id])
   end
 
   def edit
+  	#@client = Client.find(params[:id])
   end
 
   def update
   end
+
+  private
 end
