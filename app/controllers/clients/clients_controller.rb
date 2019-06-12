b97ebba3867ab73dc8b70d55f946fb0786722d7a
NTM-M <ntm.m9898@gmail.com>
2019-06-12 05:52:55 +0000

    [create]mypege2

--- app/controllers/clients/clients_controller.rb
+++ app/controllers/clients/clients_controller.rb
@@ -1,5 +1,6 @@
 class Clients::ClientsController < ApplicationController
-	befor_action :authenticate_client!
+	#befor_action :authenticate_client!
+	#befor_action :currect_client
 
 
   def show
@@ -14,4 +15,22 @@ class Clients::ClientsController < ApplicationController
   end
 
   private
+
+  #def client_params
+  	#params.require(:client).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :post_code, :telephone)
+  #end
+
+  #def correct_client
+  	#client = Client.find(params[:id])
+  	#if current_client != client
+  		#redirect_to client_path(current_client)
+  	#end
+  #end
+
+  #def logged_in_user
+  	#unless logged_in?
+  		#flash[:danger] = "Please log in."
+  		#redirect_to login_url
+  	#end
+  #end
 end
