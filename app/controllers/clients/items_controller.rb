caf8ad838988194de7eca1862c082d083c83824a
Ryosuke-Tezuka <r.tezuka.33@gmail.com>
2019-06-12 11:30:23 +0000

    [add sample_data_to_seeds.rb]

--- app/controllers/clients/items_controller.rb
+++ app/controllers/clients/items_controller.rb
@@ -1,7 +1,7 @@
 class Clients::ItemsController < ApplicationController
   def add_item
   	item = Item.new(cd_id: params[:id])
-  	item.quantity = params
+  	item.quantity = params['quantity']
     #上記、要修正
   	item.save
   	redirect_to edit_clients_path(item.id)
@@ -12,7 +12,7 @@ class Clients::ItemsController < ApplicationController
 
   def update
   	item = Item.find(params[:id])
-    item.quantity = params
+    item.quantity = params['quantity']
     #上記、要修正
     item.update
     redirect_to clients_order_buy_path(order.id)
