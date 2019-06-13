e7cd6f2924823b04ff986761cede695a3eb5c75b
NTM-M <ntm.m9898@gmail.com>
2019-06-13 10:45:49 +0000

    [add]gem paranoia

--- app/controllers/clients/clients_controller.rb
+++ app/controllers/clients/clients_controller.rb
@@ -22,8 +22,6 @@ class Clients::ClientsController < ApplicationController
   	params.require(:client).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :address, :post_code, :telephone)
   end
 
-  private
-
   def correct_client
   	client = Client.find(params[:id])
   	if current_client != client
