47be0ed5ec641bdd190686efebf13ce6c7bc0c20
sawadashutaro <shushutarou@outlook.jp>
2019-06-13 20:20:44 +0900

    repair conflict

--- app/controllers/clients/orders_controller.rb
+++ app/controllers/clients/orders_controller.rb
@@ -1,10 +1,40 @@
 class Clients::OrdersController < ApplicationController
-  def buy
-    @client = Client.find(current_user.id)
+  def buy 
+    @client = Client.find(current_client.id)
     @order = Order.new
   end
 
   def purchase
-       @order
+    post = Order.new(order_params)
+
+    if post == current_client.id
+
+      address = post.address
+      address.save
+      post_code = post.post_code
+      post_code.save
+      last_name = post.last_name
+      first_name = post.first_name
+      name = last_name + first_name
+      name.save
+
+    else
+
+     address = post.sub_address
+     address.save
+     post_code = post.sub_post_code
+     post_code.save
+     last_name = post.last_name
+     first_name = post.first_name
+     name = last_name + first_name
+     name.save
+    end
+    redirect_to clients_path
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment, :total_price, :shipping_name, :shipping_post_code, :shipping_address)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,9 @@ class Order < ApplicationRecord
 	has_many :order_items
 	belongs_to :client
 
+	enum payment:{クレジット: 0, 銀行振込: 1, 代引き: 2}
+	enum shipment_status:{準備中: 0, 発送中: 1, 配達済: 2}
+
 end
+
+
