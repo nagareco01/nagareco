c3d3883a6cad49303f8d2944d25c4f2e29cafd64
watanabe055 <mailok1212@gmail.com>
2019-06-15 02:14:57 +0000

    cds_index new creat

--- app/controllers/clients/cds_controller.rb
+++ app/controllers/clients/cds_controller.rb
@@ -3,8 +3,14 @@ class Clients::CdsController < ApplicationController
   end
 
   def index
+  	@cds = Cd.all
   end
 
   def search
+  end
+
+  	private
+  def cd_params
+    params.require(:cd).permit(:image_id)
   end
 end
