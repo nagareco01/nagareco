b9b70255fc143296c885982771cbb591be5cf370
sawadashutaro <shushutarou@outlook.jp>
2019-06-16 18:04:19 +0900

    creat purchase method

--- app/controllers/clients/orders_controller.rb
+++ app/controllers/clients/orders_controller.rb
@@ -11,15 +11,19 @@ class Clients::OrdersController < ApplicationController
   end
 
   def purchase
-    post = current_client.orders.new(order_params)
+    post = current_client.orders.new
+    post.payment = params[:order][:payment].to_i
+    post.client_id = params[:order][:client_id]
+    post.shipping_address = params[:order][:shipping_address]
 
-    if params[:client_id] = nil
+    if params[:order][:client_id] == nil
 
-      post.shipping_address = Address.find(params:[shipping_address])
-      post.shipping_post_code = Address.find(params:[shipping_address]).post_code
-      last_name = Address.find(params:[shipping_address]).last_name
-      first_name = Address.find(params:[shipping_address]).first_name
+      post.shipping_address = Address.find(params[:order][:shipping_address]).sub_address
+      post.shipping_post_code = Address.find(params[:order][:shipping_address]).sub_post_code
+      last_name = Address.find(params[:order][:shipping_address]).last_name
+      first_name = Address.find(params[:order][:shipping_address]).first_name
       post.shipping_name = last_name + first_name
+      post.client_id = current_client.id
 
     else
 
@@ -32,24 +36,40 @@ class Clients::OrdersController < ApplicationController
     end
 
     items = Item.where(client_id: current_client.id)
+    @total_price = 0
     items.each do |item|
       order_item = post.order_items.new
       order_item.cd_id = item.cd_id
       order_item.quantity = item.quantity
       order_item.puchased_price = item.price
-      total_price += item.quantity * item.price
+      @total_price += item.quantity * item.price
     end
 
-    post.total_price = total_price
+    post.payment = order_params[:payment].to_i
+    post.shipment_status = 0
+    post.total_price = @total_price
 
-    post.save
-    items.destroy
-    redirect_to clients_path
+    if post.save
+
+      items.destroy_all
+
+    else
+
+      @client = Client.find(current_client.id)
+      @order = Order.new
+      items = Item.where(client_id: current_client.id)
+      @total_price = 0
+      items.each do |item|
+        @total_price += item.quantity * item.price
+      end
+      render :buy
+
+    end
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:payment, :total_price, :shipping_name, :shipping_post_code, :shipping_address)
+    params.require(:order).permit(:payment, :client_id)
   end
 end
