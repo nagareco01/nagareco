f93d2dc3b86743e7bc84760801d6748138829249
sawadashutaro <shushutarou@outlook.jp>
2019-06-16 20:23:08 +0900

    fix purchase action

--- app/controllers/clients/orders_controller.rb
+++ app/controllers/clients/orders_controller.rb
@@ -5,7 +5,7 @@ class Clients::OrdersController < ApplicationController
     items = Item.where(client_id: current_client.id)
     @total_price = 0
     items.each do |item|
-      @total_price += item.quantity * item.price
+      @total_price += item.quantity * item.cd.price
     end
 
   end
@@ -41,8 +41,8 @@ class Clients::OrdersController < ApplicationController
       order_item = post.order_items.new
       order_item.cd_id = item.cd_id
       order_item.quantity = item.quantity
-      order_item.puchased_price = item.price
-      @total_price += item.quantity * item.price
+      order_item.purchased_price = item.cd.price
+      @total_price += item.quantity * item.cd.price
     end
 
     post.payment = order_params[:payment].to_i
