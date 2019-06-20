3ec2b0f16c9086d165e7f6867e2f10fc4fc7acfa
watanabe055 <mailok1212@gmail.com>
2019-06-20 03:44:11 +0000

    admins rework login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,7 @@ class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 	before_action :set_search
 
-	def after_sign_in_path_for(resource)
-		clients_cds_path
-	end
 
-	def after_sign_out_path_for(resource)
-		clients_cds_path
-	end
 
 	def set_search
 		@search = Cd.ransack(params[:q])
