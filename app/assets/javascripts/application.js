8abc37b52fc9ccc44a1d4bd3e0acbe050378e77a
sawadashutaro <shushutarou@outlook.jp>
2019-06-20 12:39:32 +0900

    add genre slideshow & top back bottun

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,3 +33,13 @@ jQuery(window).load(function(){
 		touchEnabled: false
 	});
 });
+
+$(function(){
+
+	$("#back a").on("click",function(){
+		$("body, html").animate({
+			scrollTop:0
+		}, 800);
+		return false;
+	});
+});
